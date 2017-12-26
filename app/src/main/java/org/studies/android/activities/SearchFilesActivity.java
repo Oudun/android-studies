29aa2cb9517e3a98206808a6c81d50478e40380e
oudun <oudun@mail.ru>
2017-12-26 14:31:29 +0300

    File search added

---
+++ app/src/main/java/org/studies/android/activities/SearchFilesActivity.java
@@ -0,0 +1,129 @@
+package org.studies.android.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.telecom.Call;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.studies.android.R;
+
+import java.io.File;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchFilesActivity extends AppCompatActivity implements Callback {
+
+    Searcher searcher;
+
+    TextView searchResultText;
+
+    @Override
+    public void searchDone() {
+//        ProgressBar progressBar = (ProgressBar) findViewById(R.id.search_progress);
+//        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void setResult(List<String> result) {
+        Log.i(this.getClass().getName(), "!!! Result found " + result);
+        searchResultText.setText(result.toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_files);
+
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.search_progress);
+
+        final EditText searchText = (EditText)  findViewById(R.id.pattern);
+
+        searchResultText = (TextView) findViewById(R.id.search_result);
+
+        Button searchButton = (Button) findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                progressBar.setVisibility(View.VISIBLE);
+                String pattern = searchText.getText().toString();
+                Log.i(this.getClass().getName(), "Looking for " + pattern);
+                searcher = new Searcher(pattern, SearchFilesActivity.this);
+                searcher.start();
+            }
+        });
+
+
+
+
+    }
+
+
+
+    class Searcher extends Thread {
+
+        List<String> result;
+        File currentDir;
+        String pattern;
+        Callback callback;
+
+        public Searcher(String str, Callback aCallback) {
+            try {
+                result = new ArrayList<String>();
+                currentDir =  new File(new URI("file:/"));
+                pattern = str;
+                callback = aCallback;
+            } catch (Exception e) {
+                Log.e(this.getClass().getName(), e.getMessage());
+            }
+        }
+
+        @Override
+        public void run() {
+            scan(currentDir);
+            callback.searchDone();
+        }
+
+        private void scan(File dirToScan) {
+            Log.d(this.getClass().getName(), "Scanning " + dirToScan);
+            if (dirToScan.getAbsolutePath().startsWith("/sys")||dirToScan.getAbsolutePath().startsWith("/proc")) {
+                Log.d(this.getClass().getName(), dirToScan + " nothing to search here");
+                return;
+            }
+            if (!dirToScan.canRead()) {
+                Log.d(this.getClass().getName(), "Can not read " + dirToScan);
+                return;
+            }
+            for (File file : dirToScan.listFiles()) {
+                if (file.getName().contains(pattern)){
+                    Log.d(this.getClass().getName(), "File " + file.getName() + " matches pattern " + pattern);
+                    result.add(file.getPath());
+                    callback.setResult(result);
+                }
+                if (file.isDirectory()) {
+                    scan(file);
+                }
+            }
+        }
+
+    }
+
+}
+
+interface Callback {
+
+    public void setResult(List<String> result);
+
+    public void searchDone();
+
+}
