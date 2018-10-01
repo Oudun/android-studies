d0e79798b2d0300bba2917285c0705a4f2ea5fcf
oudun <8adagris>
2018-10-01 20:03:03 +0300

    http post added

--- app/src/main/java/org/studies/android/activities/CameraActivity2.java
+++ app/src/main/java/org/studies/android/activities/CameraActivity2.java
@@ -2,7 +2,6 @@ package org.studies.android.activities;
 
 import android.content.Context;
 import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureFailure;
@@ -13,6 +12,9 @@ import android.media.Image;
 import android.media.ImageReader;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,7 +22,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Surface;
-import android.view.SurfaceView;
 import android.view.View;
 
 import org.studies.android.R;
@@ -30,7 +31,6 @@ import java.io.DataInputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static android.graphics.ImageFormat.JPEG;
@@ -39,8 +39,16 @@ import static android.hardware.camera2.CameraDevice.TEMPLATE_STILL_CAPTURE;
 public class CameraActivity2 extends AppCompatActivity {
 
     CaptureRequest.Builder captureRequestBuilder;
+
     CameraDevice camera;
+
     ImageReader imageReader;
+
+    Handler shooterHandler = null;
+
+    ShooterThread shooterThread;
+
+    int delay = 10000;
 
     HttpURLConnection urlConnection;
 
@@ -55,7 +63,7 @@ public class CameraActivity2 extends AppCompatActivity {
             CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
             String[] cameraList = cameraManager.getCameraIdList();
             Log.d("CAMERA_2", cameraList.toString());
-            cameraManager.openCamera(cameraList[0], deviceStateCallback, null);
+            cameraManager.openCamera(cameraList[0], deviceStateCallback, shooterHandler);
         } catch (SecurityException e) {
             Log.e("CAMERA_2", e.getMessage());
         } catch (Exception e) {
@@ -68,8 +76,11 @@ public class CameraActivity2 extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-             }
+            }
         });
+
+        shooterThread = new ShooterThread();
+        shooterThread.start();
     }
 
     CameraCaptureSession.CaptureCallback captureCallback = new CameraCaptureSession.CaptureCallback() {
@@ -94,8 +105,16 @@ public class CameraActivity2 extends AppCompatActivity {
                 image = imageReader.acquireNextImage();
                 byte[] buffer = new byte[image.getPlanes()[0].getBuffer().remaining()];
                 image.getPlanes()[0].getBuffer().get(buffer);
-                Log.d("CAMERA_2", "Buffer content is " + new String(buffer));
+                //Log.d("CAMERA_2", "Buffer content is " + new String(buffer));
+                Log.d("CAMERA_2", "Buffer content size is " + buffer.length);
                 new PostTask(buffer).execute();
+                try {
+                    CaptureRequest captureRequest = captureRequestBuilder.build();
+                    session.capture(captureRequest, captureCallback, shooterHandler);
+                    Thread.currentThread().sleep(delay);
+                } catch (Exception ee){
+                    Log.e("CAMERA_2", "Fail to sleep", ee);
+                }
             } catch (Exception e) {
                 Log.e("CAMERA_2", e.getLocalizedMessage());
             } finally {
@@ -137,10 +156,7 @@ public class CameraActivity2 extends AppCompatActivity {
             captureRequestBuilder.addTarget(imageReader.getSurface());
             CaptureRequest captureRequest = captureRequestBuilder.build();
             try {
-                session.capture(captureRequest, captureCallback, null);
-                session.capture(captureRequest, captureCallback, null);
-                session.capture(captureRequest, captureCallback, null);
-                session.capture(captureRequest, captureCallback, null);
+                session.capture(captureRequest, captureCallback, shooterHandler);
             } catch (Exception e) {
                 Log.e("CAMERA_2", "Fail to capture", e);
             }
@@ -164,11 +180,11 @@ public class CameraActivity2 extends AppCompatActivity {
             outputs.add(imageReader.getSurface());
             try {
                 captureRequestBuilder = camera.createCaptureRequest(TEMPLATE_STILL_CAPTURE);
-                camera.createCaptureSession(outputs, sessionStateCallback, null);
+                camera.createCaptureSession(outputs, sessionStateCallback, shooterHandler);
             } catch (Exception e) {
                 Log.e("CAMERA_2", "Camera error ", e);
             }
-       }
+        }
 
         @Override
         public void onDisconnected(@NonNull CameraDevice camera) {
@@ -193,27 +209,43 @@ public class CameraActivity2 extends AppCompatActivity {
         try {
             Log.i("CAMERA_2", String.format("Posting %d bytes", data.length));
             //URL url = new URL("http://192.168.137.1:8080/beholder-master/post");
-            //URL url = new URL("http://192.168.0.194:8080/beholder-master/post");
-            URL url = new URL("http://django-psql-persistent-toolbox.7e14.starter-us-west-2.openshiftapps.com");
+            URL url = new URL("http://192.168.0.194:8080/beholder-master/post");
+            //URL url = new URL("http://django-psql-persistent-toolbox.7e14.starter-us-west-2.openshiftapps.com");
             //URL url = new URL("http://ya.ru/");
 
-            urlConnection = (HttpURLConnection)url.openConnection();
+            urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setDoOutput(true);
             urlConnection.setRequestMethod("GET");
             urlConnection.getContent();
-//            ByteArrayInputStream is = new ByteArrayInputStream(data);
-//            byte[] buffer = new byte[100];
-//            while(is.read(buffer) > -1) {
-//                urlConnection.getOutputStream().write(buffer);
-//                urlConnection.getOutputStream().flush();
-//            }
-//            byte[] responseData = new byte[128];
-//            DataInputStream dis = new DataInputStream(urlConnection.getInputStream());
-//            Byte responseByte = dis.readByte();
-//            Log.i("CAMERA_2", String.format("Post result is %s", Integer.toBinaryString(responseByte)));
-            //setDelay(responseByte);
+            ByteArrayInputStream is = new ByteArrayInputStream(data);
+            byte[] buffer = new byte[100];
+            while(is.read(buffer) > -1) {
+                urlConnection.getOutputStream().write(buffer);
+                urlConnection.getOutputStream().flush();
+            }
+            byte[] responseData = new byte[128];
+            DataInputStream dis = new DataInputStream(urlConnection.getInputStream());
+            Byte responseByte = dis.readByte();
+            Log.i("CAMERA_2", String.format("Post result is %s", Integer.toBinaryString(responseByte)));
+            setDelay(responseByte);
         } catch (Exception e) {
-            Log.i("CAMERA_2", "Connection not created", e);
+            Log.i("CAMERA_2", "Connection not created");
+        }
+    }
+
+    public void setDelay(Byte delayByte) {
+
+        int delayState = delayByte & 56;
+
+        switch (delayState) {
+            case 0: { delay = 0 ; break; }
+            case 8: { delay = 1000 ; break; }
+            case 16: { delay = 5000 ; break; }
+            case 24: { delay = 10000 ; break; }
+            case 32: { delay = 30000 ; break; }
+            case 48: { delay = 60000 ; break; }
+            case 56: { delay = 150000 ; break; }
+            default: { delay = 5000;}
         }
     }
 
@@ -231,6 +263,21 @@ public class CameraActivity2 extends AppCompatActivity {
             return null;
         }
 
+    }
+
+    class ShooterThread extends Thread {
+
+        public void run() {
+            Looper.prepare();
+
+            shooterHandler = new Handler() {
+                public void handleMessage(Message msg) {
+                    // process incoming messages here
+                }
+            };
+
+            Looper.loop();
+        }
     }
 
 }
